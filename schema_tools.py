5f50b30e856ef25174aba4e92c0e76cea4fd7acc
Cindy Molitor <cindy.molitor@sagebase.org>
2019-10-14 20:55:52 +0000

    Fixed issue with the reference location dictionary

--- schema_tools.py
+++ schema_tools.py
@@ -24,6 +24,8 @@ def load_and_deref(schema_file_handle):
     import json
     import jsonschema
 
+    ref_location_dict = {}
+
     # Load the JSON schema. I am not using jsonref to resolve the $refs on load 
     # so that the $refs can point to different locations. I formerly had to pass in
     # a reference path when I was using jsonref, so all of the modules accessed by
@@ -37,12 +39,12 @@ def load_and_deref(schema_file_handle):
     for schema_key in json_schema["properties"]:
         if "$ref" in json_schema["properties"][schema_key]:
             deref_object = ref_resolver.resolve(json_schema["properties"][schema_key]["$ref"])
-            ref_location = deref_object[0]
+            ref_location_dict[schema_key] = deref_object[0]
             json_schema["properties"][schema_key] = deref_object[1]
         else:
             json_schema["properties"][schema_key] = json_schema["properties"][schema_key]
 
-    return(ref_location, json_schema)
+    return(ref_location_dict, json_schema)
 
 
 """
