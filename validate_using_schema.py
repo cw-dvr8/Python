72f63d6f29f5743036665168c81a88873239fb14
Cindy Molitor <cindy.molitor@sagebase.org>
2019-11-14 23:07:10 +0000

    Got rid of the need for a manifest file flag and added a line number for csv file errors

--- validate_using_schema.py
+++ validate_using_schema.py
@@ -31,55 +31,64 @@ def main():
                         help="Full pathname for the JSON schema file")
     parser.add_argument("validation_obj_file", type=argparse.FileType("r"),
                         help="Full pathname for the object to be validated")
-    parser.add_argument("--manifest_file", action="store_true",
-                        help="Is the object to be validated a manifest file?")
 
     args = parser.parse_args()
 
     row_error = ""
     validation_errors = ""
     data_dict_list = []
+    first_data_row = None
 
     # Load the JSON schema and create a validator..
     _, json_schema = schema_tools.load_and_deref(args.json_schema_file)
     schema_validator = jsonschema.Draft7Validator(json_schema)
 
-    # If the object to be validated is a manifest file, read it into a pandas
-    # dataframe.  Otherwise, read it into JSON.
-    if args.manifest_file:
-        data_file_df = pd.read_csv(args.validation_obj_file)
-
-        # Pandas reads in empty fields as nan. Replace nan with None.
-        data_file_df = data_file_df.replace({pd.np.nan: None})
-
-        data_dict_list = data_file_df.to_dict(orient="records")
-
-        # The first row of a manifest file (csv) that contains actual data will
-        # be row 2 (header is line 1), so add 2 to the python row counter.
-        first_data_row = 2
-
-    else:
+    # Attempt to read the file to be validated as a JSON file.
+    try:
         json_file_dict = json.load(args.validation_obj_file)
         data_dict_list.append(json_file_dict)
 
-        # A JSON file is considered to only have one row of data.
-        first_data_row = 1
+    except:
+        # If the attempt to read the file as JSON fails, attemp to read it as
+        # a csv.
+        try:
+            args.validation_obj_file.seek(0)
+            data_file_df = pd.read_csv(args.validation_obj_file)
 
-    for row_number, data_record in enumerate(data_dict_list):
+            # Pandas reads in empty fields as nan. Replace nan with None.
+            data_file_df = data_file_df.replace({pd.np.nan: None})
 
-        # Remove any None values from the dictionary - it simplifies the coding of the
-        # JSON validation schema.
+            data_dict_list = data_file_df.to_dict(orient="records")
+
+            # The first line of a manifest file (csv) that contains actual data
+            # will be row 2 (header is line 1).
+            first_data_row = 2
+
+        except:
+            raise Exception ("Problem with file to be validated") from None
+
+    for line_number, data_record in enumerate(data_dict_list):
+
+        # Remove any None values from the dictionary - it simplifies the
+        # coding of the JSON validation schema.
         clean_record = {k: data_record[k] for k in data_record if data_record[k] is not None}
 
-        # We are not currently allowing multiple types in reference definitions, so convert
-        # Booleans to strings if the key is also allowed to contain string values.
+        # We are not currently allowing multiple types in reference
+        # definitions, so convert Booleans to strings if the key is also
+        # allowed to contain string values.
         converted_clean_record = schema_tools.convert_from_other(clean_record,
                                           json_schema,
                                           schema_tools.convert_bool_to_string)
 
         schema_errors = schema_validator.iter_errors(converted_clean_record)
 
-        row_error = schema_tools.validation_errors(schema_errors)
+        if first_data_row:
+            line_number = "Line " + str(first_data_row + line_number) + ": "
+            row_error = schema_tools.validation_errors(schema_errors,
+                                                   line_prepend=line_number)
+        else:
+            row_error = schema_tools.validation_errors(schema_errors)
+
         if row_error:
             validation_errors += row_error
 
