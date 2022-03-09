f23cdd2db71fc02bc52aff6b4a50230b09793d4e
Cindy Molitor <cindy@fredhutch.org>
2022-03-09 10:49:45 -0800

    Updated comments

--- change_ptid_to_pubid.py
+++ change_ptid_to_pubid.py
@@ -18,9 +18,6 @@ Execution: python change_ptid_to_pubid.py <look-up file>
 import argparse
 import pandas as pd
 
-"""
-Function: main
-"""
 def main():
 
     parser = argparse.ArgumentParser()
@@ -36,13 +33,21 @@ def main():
 
     lookup_df = pd.read_csv(args.lookup_file)
     ids_df = lookup_df[["ptid", "pubid"]].copy()
+
+    # PTIDs in this file may have the format "XXX-XX-XXXX" or something
+    # similar, so eliminate the dashes if necessary.
     ids_df["ptid"] = ids_df["ptid"].str.replace("-", "")
     ids_df =ids_df.rename(columns={"ptid": "PTID", "pubid": "PUBID"})
 
     data_df = pd.read_csv(args.data_file, sep="\t")
+    # Make sure that the PTID is a string value instead of an int.
     data_df["PTID"] = data_df["PTID"].astype(str)
 
     combined_df = pd.merge(data_df, ids_df, how="left", on="PTID", indicator=True)
+
+    # Get a list of the column headers, replace "PTID" with "PUBID" so that
+    # the PUBID will appear in the correct column order, and then use the
+    # list to subset the data frame.
     column_list = combined_df.columns.values.tolist()
     column_list.remove("PUBID")
     column_list.remove("_merge")
@@ -51,6 +56,7 @@ def main():
 
     combined_df.to_csv(args.out_file, sep="\t", index=False)
 
+    # Look for any instances where there is no PUBID.
     no_pubid_df = combined_df.loc[combined_df["PUBID"] == ""]
     if len(no_pubid_df) > 0:
         print("PTIDs without PUBIDs\n")
